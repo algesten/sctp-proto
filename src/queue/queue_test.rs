b148692d807d26f9f37fd30f973e6e41f1e57640
Woodrow Douglass <wdouglass@carnegierobotics.com>
2026-07-06 09:03:50 -0400

    Tests for wrapping TSNs in reassembly queue

--- src/queue/queue_test.rs
+++ src/queue/queue_test.rs
@@ -1036,3 +1036,67 @@ fn test_reassembly_queue_ssn_overflow_in_forward_tsn_for_ordered() -> Result<()>
 
     Ok(())
 }
+
+#[test]
+fn test_chunk_set_wrap() {
+    let cset = Chunks::new(
+        0,
+        PayloadProtocolIdentifier::default(),
+        vec![
+            ChunkPayloadData {
+                tsn: u32::MAX - 1,
+                beginning_fragment: true,
+                ..Default::default()
+            },
+            ChunkPayloadData {
+                tsn: u32::MAX,
+                ..Default::default()
+            },
+            ChunkPayloadData {
+                tsn: 0,
+                ending_fragment: true,
+                ..Default::default()
+            },
+        ],
+    );
+
+    assert!(
+        cset.is_complete(),
+        "chunkSet with wrapping TSNs is not complete"
+    );
+}
+
+#[test]
+fn test_reassembly_queue_wrap_find_complete() {
+    let mut rq = ReassemblyQueue::new(0, 65535);
+
+    assert!(rq
+        .push(ChunkPayloadData {
+            tsn: u32::MAX - 1,
+            unordered: true,
+            beginning_fragment: true,
+            ..Default::default()
+        })
+        .is_ok());
+    assert!(rq
+        .push(ChunkPayloadData {
+            tsn: u32::MAX,
+            unordered: true,
+            ..Default::default()
+        })
+        .is_ok());
+    assert!(rq
+        .push(ChunkPayloadData {
+            tsn: 0,
+            unordered: true,
+            ending_fragment: true,
+            ..Default::default()
+        })
+        .is_ok());
+
+    assert_eq!(
+        rq.unordered.len(),
+        1,
+        "chunkSet with wrapping TSNs is not complete"
+    );
+}
